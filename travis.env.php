c4d651b8eb533335975f2e5edee4690f4ab80de4
Ryan Raub <ryan.raub@asu.edu>
2015-11-20 15:22:31 -0700

    trying another .env config for travis

--- travis.env.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    'DB_HOST'     => '127.0.0.1',
-    'DB_DATABASE' => 'gatherup',
-    'DB_USERNAME' => 'travis',
-    'DB_PASSWORD' => '',
-    'APP_KEY'     => 'g9FUQV0KLjmug8aKLRAMZQynwk9PxM92',
-];
