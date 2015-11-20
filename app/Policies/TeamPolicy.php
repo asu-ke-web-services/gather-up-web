c161a1edc1a6922ea6292e3ee3f07fb3b4c4577a
Ivan Montiel <idmontie@asu.edu>
2015-11-20 16:12:25 -0700

    Some clean up

--- app/Policies/TeamPolicy.php
+++ app/Policies/TeamPolicy.php
@@ -17,7 +17,7 @@ class TeamPolicy
     const ERROR_MESSAGE = 'You have reached the maximum number of teams for your plan';
 
     /**
-     * Create a new policy instance.
+     * Create a new policy instance. Will create a new validator.
      *
      * @return void
      */
@@ -26,6 +26,17 @@ class TeamPolicy
         Validator::extend('canMakeTeam', 'GatherUp\Policies\TeamPolicy@canMakeTeam', self::ERROR_MESSAGE);
     }
 
+    /**
+     * Returns an array. By convention, the key is the value that
+     * will be checked, in this case, we will not actually be checking
+     * the "name" key of the given request object. We only require that
+     * it exists. This is sort of hacky. We can make it so that
+     * the validator is implicit (in the constructor) and will also be run,
+     * or we can just require name to be set and then have the `canMakeTeam`
+     * validator run.
+     * 
+     * @return array
+     */
     public function validateNewTeamsWith()
     {
         return [
@@ -33,7 +44,16 @@ class TeamPolicy
         ];
     }
 
-    public function canMakeTeam($attribute, $userId, $parameters, $validator)
+    /**
+     * Will check if the given user can make a team, this
+     * is based on a few rules:
+     *
+     * 1. Whether the plan is active
+     * 2. The plan type
+     * 
+     * @return Boolean Whether the user can make a team
+     */
+    public function canMakeTeam()
     {
         $user = User::findOrFail(Auth::id());
         $numberOfTeams = $user->getNumberOfTeams();
