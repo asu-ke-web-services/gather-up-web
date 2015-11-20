bf5556fb2da2dce0cadb0cb9227c167000471213
Ryan Raub <ryan.raub@asu.edu>
2015-11-20 15:13:21 -0700

    wrong db user for travis

--- travis.env.php
+++ travis.env.php
@@ -2,7 +2,7 @@
 return [
     'DB_HOST'         => '127.0.0.1',
     'DB_NAME'         => 'gatherup',
-    'DB_USER'         => 'root',
+    'DB_USER'         => 'travis',
     'DB_PASS'         => '',
     'APP_KEY'         => 'g9FUQV0KLjmug8aKLRAMZQynwk9PxM92',
 ];
