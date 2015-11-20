e1a39a58d283f5212d51c42a1aa4fdb4fd38faa2
Ryan Raub <ryan.raub@asu.edu>
2015-11-20 15:09:07 -0700

    added app key to travis test environment

--- travis.env.php
+++ travis.env.php
@@ -3,5 +3,6 @@ return [
     'DB_HOST'         => '127.0.0.1',
     'DB_NAME'         => 'gatherup',
     'DB_USER'         => 'root',
-    'DB_PASS'         => ''
+    'DB_PASS'         => '',
+    'APP_KEY'         => 'g9FUQV0KLjmug8aKLRAMZQynwk9PxM92',
 ];
